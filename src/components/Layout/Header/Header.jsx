a9e62e262743eef6152c7027f8a9d88f7dfd2415
bspraveen <billasujith77@gmail.com>
2023-10-02 20:41:54 +0530

    Changing the favicon and title of the page

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>React Meals</h1>
+        <h1>Ichiraku no Inshoku-ten</h1>
         <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
